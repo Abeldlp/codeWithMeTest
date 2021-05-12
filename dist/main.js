d284c357105ca265fe72389267180cf3c09533b6
Abel de la Paz <abel@foodticket.nl>
2021-05-12 23:07:57 +0200

    Test commit to try out code with me

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+var age = 23;
+var people = [
+    'John',
+    'Rupert',
+    'Leo'
+];
+function regretPeople(name) {
+    fetch('https://www.abeldlp.com')
+        .then(function (res) { return console.log(res); });
+}
+regretPeople('Marilin');
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+let age : number = 23;
+let people : any = [
+    'John',
+    'Rupert',
+    'Leo'
+]
+
+function regretPeople(name:string) {
+    fetch('https://www.abeldlp.com')
+        .then(res => console.log(res))
+  
+}
+
+regretPeople('Marilin');
